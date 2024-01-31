bb7ab49a2217f03d08a4efc772fe46e19679012c
razeensajeel04 <razeen.sajeel@xcubated.com>
2024-01-31 13:59:55 +0530

    second card

--- xbs-care/src/dashboard.js
+++ xbs-care/src/dashboard.js
@@ -6,9 +6,11 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Badge } from 'primereact/badge';
 import { Avatar } from 'primereact/avatar';
+import { Carousel } from 'primereact/carousel';
+
 
 const Dashboard = () => {
-  const [statuses] = useState(['unqualified', 'qualified', 'new', 'negotiation', 'renewal']);
+  const [statuses] = useState(['unscheduled', 'scheduled', 'new', 'negotiation', 'renewal']);
 
   const getSeverity = (status) => {
     switch (status) {
@@ -41,6 +43,13 @@ const Dashboard = () => {
     width: '100%',
   };
 
+  const cardStyleweekly = {
+    borderRadius: '10px',
+    border: '2px solid #41b5d1',
+    width: '90%',
+
+  };
+
   const userInfoStyle = {
     display: 'flex',
     alignItems: 'center',
@@ -57,7 +66,18 @@ const Dashboard = () => {
   const tableData = [
     { firstname: 'Razeen', lastname: 'Sajeel', date: '2024-02-01', time: '10:00 AM', status: 'unscheduled' },
     { firstname: 'Panchami', lastname: 'Nair', date: '2024-02-02', time: '2:30 PM', status: 'scheduled' },
-    
+    { firstname: 'Febin', lastname: 'Thomas', date: '2024-02-02', time: '2:30 PM', status: 'scheduled' },
+  ];
+
+  const carouselItems = [
+    { content: 'Required Hours', icon: 'pi pi-clock', color: '#4169E1', hours: '00hr : 51min' },
+    { content: 'Booked Hours', icon: 'pi pi-clock', color: '#4169E1', hours: '01hr : 25min' },
+    { content: 'Pending Hours', icon: 'pi pi-clock', color: '#4169E1', hours: '02hr : 10min' },
+  ];
+
+  const responsiveOptions = [
+    { breakpoint: '1024px', numVisible: 2, numScroll: 2 },
+    { breakpoint: '768px', numVisible: 1, numScroll: 1 },
   ];
 
   return (
@@ -72,9 +92,9 @@ const Dashboard = () => {
         <Container style={{ marginTop: '60px' }}>
           <i className="pi pi-home" style={{ color: '#41b5d1' }}></i> <span style={{ color: '#41b5d1' }}>Dashboard</span>
           <Row>
-            <Col>
-              <Card title='Unassigned Appointment'>
-                <DataTable value={tableData} tableStyle={{ minWidth: '50rem' }}>
+            <Col md={7} >
+              <Card title='Unassigned Appointments' style={cardStyle}>
+                <DataTable value={tableData} tableStyle={{ minWidth: '40rem' }}>
                   <Column field="firstname" header="First Name"></Column>
                   <Column field="lastname" header="Last Name"></Column>
                   <Column field="date" header="Date"></Column>
@@ -94,20 +114,36 @@ const Dashboard = () => {
                 </DataTable>
               </Card>
             </Col>
-            <Col>
-              <Card title='card1' style={cardStyle}>
-                <p>Card content goes here.</p>
+            <Col md={5}>
+              <Card title='Weekly Status' style={cardStyle}>
+                <Carousel value={carouselItems} numScroll={1} numVisible={2} responsiveOptions={responsiveOptions} itemTemplate={(item) => (
+                  <div>
+                    <Card style={cardStyle}>
+                      <div className='text-center'>
+                        <i className={item.icon} style={{ fontSize: '3.5rem', color: item.color }}></i>
+                      </div>
+                      <h5 className="m-0 text-center">{item.content}</h5>
+                      <p className="m-0 text-center">{item.hours}</p>
+                    </Card>
+                  </div>
+                )} />
               </Card>
             </Col>
           </Row>
           <Row>
             <Col>
-              <Card title='card1' style={cardStyle}>
-                <p>Card content goes here.</p>
+              <Card title='Training Requirement' style={cardStyle}>
+                <col md={6} >
+                <Card>
+
+               
+                  </Card> 
+                </col>
+                
               </Card>
             </Col>
             <Col>
-              <Card title='card1' style={cardStyle}>
+              <Card title='Card 2' style={cardStyle}>
                 <p>Card content goes here.</p>
               </Card>
             </Col>
